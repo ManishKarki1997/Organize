20c22e66b634da65e1c967f0181b2357bd8d4e42
ManishKarki1997 <manishkarki247@gmail.com>
2021-11-07 12:25:35 +0545

    add electron

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./assets/styles/main.css";
+import "vfonts/IBMPlexSans.css";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
